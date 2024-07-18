a1cf81ff29a36caa7fb468e6079ecbb2e230908e
Al Priest <alpriest@gmail.com>
2024-07-18 20:54:39 +0100

    Read local config file

--- export-import/update_config.rb
+++ export-import/update_config.rb
@@ -6,10 +6,8 @@ require 'json'
 beers = JSON.parse(STDIN.read)
 
 # Modify the existing config
-open('../config.json') do |original_file|
-  original_json = JSON.parse(original_file.read)
-  original_json[:brews] = beers
-  updated_json = JSON.pretty_generate(original_json)
-end
+original_json = JSON.parse(File.read('../config.json'))
+original_json[:brews] = beers
+updated_json = JSON.pretty_generate(original_json)
 
-update_json
+puts update_json

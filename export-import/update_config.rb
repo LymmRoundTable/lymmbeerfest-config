239d2891cb1f2d59a2351bf474afc4a93a62aeb5
Al Priest <alpriest@gmail.com>
2024-07-18 21:15:01 +0100

    Increment version number on update

--- export-import/update_config.rb
+++ export-import/update_config.rb
@@ -10,6 +10,7 @@ file_contents = File.read('../config.json')
 
 original_json = JSON.parse(file_contents)
 original_json['brews'] = beers
+original_json['version'] = original_json['version'].to_i + 1
 updated_json = JSON.pretty_generate(original_json)
 
 puts updated_json

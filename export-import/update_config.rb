7518416d193f0e2375225ccc8c8bc8f7ea80b7db
Al Priest <alpriest@gmail.com>
2024-07-18 21:13:31 +0100

    Remove duplicate brews key

--- export-import/update_config.rb
+++ export-import/update_config.rb
@@ -9,7 +9,7 @@ beers = JSON.parse(STDIN.read)
 file_contents = File.read('../config.json')
 
 original_json = JSON.parse(file_contents)
-original_json[:brews] = beers
+original_json['brews'] = beers
 updated_json = JSON.pretty_generate(original_json)
 
 puts updated_json

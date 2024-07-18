253fd9236a03ae01666e8201d43225611f17c0a0
Al Priest <alpriest@gmail.com>
2024-07-18 21:07:01 +0100

    Automatic update from Google sheet

--- update.sh
+++ update.sh
@@ -5,6 +5,6 @@ cd export-import
 ruby export_from_sheets.rb | ruby update_config.rb > ../config_updated.json
 cd ..
 mv config_updated.json config.json
-# git add config.json
-# git commit -am "Automatic update from Google sheet"
-# git push
+git add config.json
+git commit -am "Automatic update from Google sheet"
+git push

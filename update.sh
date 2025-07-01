b5487ca22d512326375220061914a577ee8dcf78
Al Priest <alpriest@gmail.com>
2025-07-01 11:43:39 +0100

    Automatic update from CSV

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@
 
 git pull
 cd export-import
-ruby convert_csv_to_json.rb | ruby update_config.rb
-mv config_updated.json config.json
+ruby convert_csv_to_json.rb | ruby update_config.rb > config_updated.json
+mv config_updated.json ../config.json
 git add config.json
 git commit -am "Automatic update from CSV"
 git push
